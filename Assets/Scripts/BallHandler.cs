94cc743ff4a1adfb0b20cf2c5e3573b4e084d380
Garrett Crook <GRCrook03@icloud.com>
2022-08-06 14:23:55 -0400

    Ball launcher knocks down tower

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,53 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] Rigidbody2D currentBallRigidBody;
+    [SerializeField] SpringJoint2D currentBallSpringJoint;
+    [SerializeField] float delayDuration = 0.5f;
+    
+    private Camera mainCamera;
+    private bool isDragging = false;
 
+    private void Start() 
+    {
+        mainCamera = Camera.main;
+    }
     
     void Update()
     {
+        if (currentBallRigidBody == null) {return;}
+        
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+            
+            isDragging =  false;
             return;
         }
         
+        isDragging = true;
+        currentBallRigidBody.isKinematic = true;
         Vector2 touchPosition =  Touchscreen.current.primaryTouch.position.ReadValue();
         
-        Debug.Log(touchPosition);
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidBody.position = worldPosition;
+    }
+
+     void LaunchBall()
+    {
+        currentBallRigidBody.isKinematic = false;
+        currentBallRigidBody = null;
+        
+        Invoke(nameof(DetachBall), delayDuration);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
